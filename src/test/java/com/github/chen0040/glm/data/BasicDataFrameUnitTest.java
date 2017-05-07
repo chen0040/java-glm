206b1841a1a1ba4b5d6ff0012ced5cd04a915738
xschen <xs0040@gmail.com>
2017-05-07 11:27:34 +0800

    Coverage unit test for the data row class

---
+++ src/test/java/com/github/chen0040/glm/data/BasicDataFrameUnitTest.java
@@ -0,0 +1,18 @@
+package com.github.chen0040.glm.data;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+
+/**
+ * Created by xschen on 7/5/2017.
+ */
+public class BasicDataFrameUnitTest {
+
+   @Test
+   public void test_add_row(){
+
+   }
+}
